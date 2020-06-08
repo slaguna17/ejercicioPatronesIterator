e1565e2bdd128d758f6fdb8cbc7fa7c3f31dfc70
slaguna17 <43455115+slaguna17@users.noreply.github.com>
2020-06-08 01:23:19 -0400

    Add files via upload
    Ejercicios 3

---
+++ ejercicios3_SergioLaguna/ej2/Client.java
@@ -0,0 +1,10 @@
+package ejercicios3_SergioLaguna.ej2;
+
+
+public class Client {
+    public static void main(String[] args) {
+        Organizador organizador = new Organizador();
+
+        organizador.criteriaHandler(true,false,false);
+    }
+}
